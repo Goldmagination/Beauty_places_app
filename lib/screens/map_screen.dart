e9e09e80657fd259d35090dc26b497719f0a33e5
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2020-08-21 19:43:26 +0200

    google maps handling

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -17,11 +17,30 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Location'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedLocation);
+                    },
+            )
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -29,6 +48,12 @@ class _MapScreenState extends State<MapScreen> {
               widget.initialLocation.longitude),
           zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? null
+            : {
+                Marker(markerId: MarkerId('x1'), position: _pickedLocation),
+              },
       ),
     );
   }
